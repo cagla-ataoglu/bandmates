6e5526c2aa210031299c5d0262732ca72dcea206
piarosebelledelapaz <piadelapaz999@gmail.com>
2024-03-17 12:23:35 +0100

    added post service

---
+++ post-service/services/post_service.py
@@ -0,0 +1,50 @@
+import boto3
+
+class PostService:
+    def __init__(self):
+        self.dynamodb = boto3.resource('dynamodb', endpoint_url='http://localstack:4566')
+        self.table_name = 'Posts'
+
+        try:
+            self.dynamodb.Table(self.table_name).load()
+            print(f"Table '{self.table_name}' already exists. Loading it.")
+        except self.dynamodb.meta.client.exceptions.ResourceNotFoundException:
+            table = self.dynamodb.create_table(
+                TableName=self.table_name,
+                KeySchema=[{'AttributeName': 'PostId', 'KeyType': 'HASH'}],
+                AttributeDefinitions=[{'AttributeName': 'PostId', 'AttributeType': 'S'}],
+                ProvisionedThroughput={'ReadCapacityUnits': 5, 'WriteCapacityUnits': 5}
+            )
+            table.meta.client.get_waiter('table_exists').wait(TableName=self.table_name)
+            print("Posts table created.")
+        except Exception as e:
+            raise RuntimeError(f"Error initializing DynamoDB table: {e}")
+
+        self.posts_table = self.dynamodb.Table(self.table_name)
+
+    def create_post(self, post_id, title, content, author_id, timestamp, video_url):
+        try:
+            self.posts_table.put_item(
+                Item={
+                    'PostId': post_id,
+                    'Title': title,
+                    'Content': content,
+                    'AuthorId': author_id,
+                    'Timestamp': timestamp,
+                    'VideoUrl': video_url
+                }
+            )
+            print('Post created successfully.')
+        except Exception as e:
+            raise RuntimeError(f"Error creating post: {e}")
+
+    def get_post(self, post_id):
+        try:
+            response = self.posts_table.get_item(Key={'PostId': post_id})
+            if 'Item' in response:
+                return response['Item']
+            else:
+                return None
+        except Exception as e:
+            raise RuntimeError(f"Error retrieving post: {e}")
+
