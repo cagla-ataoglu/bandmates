cb0d4dbdac201e610702689e60826ae9c56404de
efesaman <63561602+efesaman@users.noreply.github.com>
2024-03-17 23:58:09 +0100

    Added storage-service
    - Storage service allows media upload to s3 bucket.
    - Small bug fix with AuthController

--- auth-service/controller.py
+++ auth-service/controller.py
@@ -51,4 +51,4 @@ class AuthController:
 if __name__ == '__main__':
     cherrypy.config.update({'server.socket_host': '0.0.0.0'})
     cherrypy.config.update({'server.socket_port': 8080})
-    cherrypy.quickstart(AuthService())
+    cherrypy.quickstart(AuthController())
