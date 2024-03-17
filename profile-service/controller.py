774f3d585f3298b8fc2c0f78d45d2d3c13607120
Çağla Ataoğlu <63456419+cagla-ataoglu@users.noreply.github.com>
2024-03-17 16:56:57 +0100

    edit profile service controller
    - remove dynamo db for now
    - update config for profile service

--- profile-service/controller.py
+++ profile-service/controller.py
@@ -1,12 +1,16 @@
 import cherrypy
-from services.dynamodb_service import DynamoDBService
 
 class ProfileService:
 
     def __init__(self):
-        self.dynamodb_service = DynamoDBService()
+        pass
 
     @cherrypy.expose
-    @cherrypy.tools.json_out
+    @cherrypy.tools.json_out()
     def index(self):
         return {"message": "Welcome to Profile Service"}
+    
+if __name__ == '__main__':
+    cherrypy.config.update({'server.socket_host': '0.0.0.0'})
+    cherrypy.config.update({'server.socket_port': 8081})
+    cherrypy.quickstart(ProfileService())
