c568e50d28d059c8ba04f61030a331b220f89d6c
Yaren Durgun <yaren.durgun@ug.bilkent.edu.tr>
2024-04-24 23:45:55 +0200

    minor style changes to components and pages

--- bandmates-app/src/components/ProfileCard/ProfileCard.jsx
+++ bandmates-app/src/components/ProfileCard/ProfileCard.jsx
@@ -8,9 +8,12 @@ const ProfileCard = () => {
                 <div className="gradiant"></div>
                 <div className="profile-down">
                     <img src="" alt="" />
-                    <div className="profile-title">Profile title here</div>
-                    <div className="profile-description">Profile information here</div>
-                    <div className="profile-button"><a href="mailto: user@email">Contact Me (link to email here)</a></div>
+                    <div className="profile-title">Name</div> {/* username or display name */}
+                    <div className="profile-information">Profile Type</div> {/* band or musician */}
+                    <div className="profile-information">Location</div>
+                    <div className="profile-description">Profile Description</div> {/* a description of their profile given by the user */}
+                    <div className="profile-button"><a href="mailto: user@email">Contact Me</a></div> {/* put here the link to msg function or email */}
+
                 </div>
             </div>
         </div>
--- bandmates-app/src/pages/Profile/Profile.jsx
+++ bandmates-app/src/pages/Profile/Profile.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
-import Sidebar from '../../components/Sidebar/Sidebar'
-import Rightbar from '../../components/Rightbar/Rightbar'
 import ProfileCard from '../../components/ProfileCard/ProfileCard'
 
 const Profile = () => {
