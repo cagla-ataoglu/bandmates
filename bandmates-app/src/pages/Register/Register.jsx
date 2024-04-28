82062c9d952df4fa36db6a1f25a95638fcda046f
efesaman <63561602+efesaman@users.noreply.github.com>
2024-04-28 21:04:57 +0200

    Added register logic to the frontend
    We make a backend call to register the user.

--- bandmates-app/src/pages/Register/Register.jsx
+++ bandmates-app/src/pages/Register/Register.jsx
@@ -1,8 +1,67 @@
-import React from 'react';
-import './Register.css'; // Import the CSS file for Register styling
+import React, { useState } from 'react';
+import './Register.css';
 import Logo from '../../components/Logo/Logo';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSignUp = async () => {
+    if (password !== confirmPassword) {
+      alert('Passwords do not match');
+      return;
+    }
+
+    try {
+      const signup_response = await fetch('http://localhost:8080/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: username,
+          email: email,
+          password: password
+        })
+      });
+
+      const data = await signup_response.json();
+
+      if (signup_response.ok) {
+        const signin_response = await fetch('http://localhost:8080/signin', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            username: username,
+            password: password
+          })
+        });
+
+        const signin_data = await signin_response.json();
+
+        if (signin_response.ok) {
+          localStorage.setItem('access_token', signin_data.tokens.access_token);
+          localStorage.setItem('refresh_token', signin_data.tokens.refresh_token);
+          localStorage.setItem('id_token', signin_data.tokens.id_token);
+          navigate('/');
+        } else {
+          alert('Signin failed.');
+          navigate('/login');
+        }
+      } else {
+        alert('Signup failed.');
+      }
+    } catch (error) {
+      alert(`Error: ${error.message}`);
+    }
+  };
+
   return (
     <div className="register-container">
       <div className="content">
@@ -12,12 +71,36 @@ const Register = () => {
         </div>
         <div className="right-content">
           <div className="form-container">
-            <input type="name" placeholder="username" className="input-field" />
-            <input type="email" placeholder="email" className="input-field" />
-            <input type="password" placeholder="password" className="input-field" />
-            <input type="password" placeholder="confirm password" className="input-field" />
-            <button className="button">Sign Up</button>
-            <button className="button secondary">Already have an account? Login</button>
+            <input
+              type="name"
+              placeholder="username"
+              className="input-field"
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+            />
+            <input
+              type="email"
+              placeholder="email"
+              className="input-field"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="password"
+              className="input-field"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="confirm password"
+              className="input-field"
+              value={confirmPassword}
+              onChange={e => setConfirmPassword(e.target.value)}
+            />
+            <button className="button" onClick={handleSignUp}>Sign Up</button>
+            <button className="button secondary" onClick={() => {navigate('/login')}}>Already have an account? Login</button>
           </div>
         </div>
       </div>
